008d190d31e1762a582576954e85879afc55abbc
ShawnWolgu <garaireen@sjtu.edu.cn>
2022-09-02 14:17:15 +0800

    just a update

--- pythonscript/plot_case.py
+++ pythonscript/plot_case.py
@@ -6,16 +6,25 @@ from snplot.snplot import plotdata
 
 def getplot_database(folders,filename,xcol,ycol):
     database = []
+    xlim = [0,0]
+    ylim = [0,0]
     for ifol in folders:
         folderp = os.path.join(os.getcwd(),ifol)
         filep = os.path.join(folderp, filename)
         df = pd.read_csv(filep)
-        dfcsv = pd.concat([df.iloc[:,xcol],df.iloc[:,ycol]],axis=1)
+        interval = max(round(len(df.iloc[:,xcol])/200),1)
+        dfcsv = pd.concat([df.iloc[::interval,xcol],df.iloc[::interval,ycol]],axis=1)
+        xlim[0] = min(dfcsv.iloc[0,0],xlim[0])
+        xlim[1] = max(dfcsv.iloc[-1,0],xlim[1])
+        ylim[0] = min(dfcsv.iloc[0,1],ylim[0])
+        ylim[1] = max(dfcsv.iloc[-1,1],ylim[1])
         database.append({'linedata':dfcsv,'label':ifol})
-    return database
+    return database, xlim, ylim
 
 folders = sys.argv[1:]
-plotdata(getplot_database(folders,'stress_grain.csv',3,9),'stress')
-plotdata(getplot_database(folders,'stress_step.csv',0,9),'stress_step',xlim=(0,25),ylim=(0,800))
+datab,xlim,ylim = getplot_database(folders,'stress_grain.csv',3,9)
+plotdata(datab,'stress',xlim=(xlim[0],xlim[1]),ylim=(ylim[0],1.2*ylim[1]))
+datab,xlim,ylim = getplot_database(folders,'stress_step.csv',0,9)
+plotdata(datab,'stress_step',xlim=(xlim[0],xlim[1]),ylim=(ylim[0],1.2*ylim[1]))
 
 
