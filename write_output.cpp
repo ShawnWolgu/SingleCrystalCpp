ec2d5ee9ddd1ab8075e5145f821e47df720577b9
ShawnWolgu <garaireen@sjtu.edu.cn>
2022-11-13 20:31:28 +0800

    cross slip, debug ver.

--- write_output.cpp
+++ write_output.cpp
@@ -1,10 +1,13 @@
 #include "singleX.h"
 
+void custom_output_initialization(Grain &grain);
 void custom_output_initialization();
 void print_custom(Grain &grain);
 
 void title_output(ofstream &outf, string leads, string subs, int length);
 void title_output(ofstream &outf, string leads, string subs1, string subs2, int length);
+double cal_std_013467910(vector<double> vd);
+double cal_mean_013467910(vector<double> vd);
 
 void outfile_initialization(){
     cout << "Output Files Initialing " << endl;
@@ -48,7 +51,7 @@ void outfile_initialization(Grain &grain){
     grain.print_schmidt(schmidt_file);
     title_output(disvel_file, "e11,e22,e33,", "vel", slip_num);
     grain.print_disvel(disvel_file);
-    custom_output_initialization();
+    custom_output_initialization(grain);
     cout << "Finish Initialization." << endl;
 }
 
@@ -67,6 +70,7 @@ void substep_output(Grain &grain){
     grain.print_stress_strain(stress_step_file);
     grain.print_dislocation(disloc_step_file);
     if (flag_harden == 2) grain.print_time(time_step_file);
+    print_custom(grain);
 }
 
 void grain_output(Grain &grain){
@@ -103,13 +107,42 @@ void title_output(ofstream &outf, string leads, string subs1, string subs2, int 
 
 // custom output settings:
 void custom_output_initialization(){
-    title_output(custom_output_file, "e11,e22,e33,", "cross", 12);
+    //title_output(custom_output_file, "e11,e22,e33,", "rate", 12);
     //custom_output_file << "e11,e22,e33,U11,U12,U13,U21,U22,U23,U31,U32,U33" << endl;
+    custom_output_file << "e11,e22,e33,std_ssd,std_b,std_rate" << endl;
+}
+
+void custom_output_initialization(Grain &grain){
+    //title_output(custom_output_file, "e11,e22,e33,", "rate", 12);
+    //custom_output_file << "e11,e22,e33,U11,U12,U13,U21,U22,U23,U31,U32,U33" << endl;
+    custom_output_file << "e11,e22,e33,std_ssd,std_b,std_rate" << endl;
+    print_custom(grain);
 }
 
 void print_custom(Grain &grain){
     custom_output_file << grain.strain_tensor(0,0) << ',' << grain.strain_tensor(1,1) << ',' << grain.strain_tensor(2,2);// << ',' << grain.slip_sys[4].update_params[0];
     //Matrix3d U = (grain.orientation.transpose() * grain.orient_ref).inverse()*grain.deform_grad_elas;
-    for (Slip &slip_component : grain.slip_sys) custom_output_file << ',' << slip_component.cross_in - slip_component.cross_out;
-    custom_output_file << endl;
+    vector<double> ssd, bb, rate;
+    for (Slip &slip_component : grain.slip_sys) {
+	ssd.push_back(slip_component.SSD_density);
+	bb.push_back(slip_component.harden_params[0]);
+	rate.push_back(slip_component.strain_rate_slip);
+	//custom_output_file << ',' << slip_component.strain_rate_slip;// cross_in - slip_component.cross_out;
+    }
+    double ssd_std = cal_std_013467910(ssd);
+    double bb_std = cal_std_013467910(bb);
+    double rate_std = cal_std_013467910(rate); 
+    custom_output_file << ',' << ssd_std << ',' << bb_std << ',' << rate_std << endl;
+}
+
+double cal_std_013467910(vector<double> vd){
+    double mean = cal_mean_013467910(vd);
+    int idx[8] = {0,1,3,4,6,7,9,10};
+    double add_number = 0.0;
+    for (auto &iid : idx) add_number += pow((abs(vd[iid])-mean),2);
+    return sqrt(0.125*add_number)/mean;
+}
+
+double cal_mean_013467910(vector<double> vd){
+    return 0.125 * (abs(vd[0]) + abs(vd[1]) + abs(vd[3])+ abs(vd[4]) + abs(vd[6]) + abs(vd[7]) + abs(vd[9]) + abs(vd[10]));
 }
